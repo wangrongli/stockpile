6f913fc03e326d66c8a22415eea8a50b8e94aff2
wangrongli <wangrongli@daojia-inc.com>
2018-06-17 11:50:02 +0800

    test

--- workDemo/src/main/java/com/wangrl/workDemo/App.java
+++ workDemo/src/main/java/com/wangrl/workDemo/App.java
@@ -6,6 +6,6 @@ package com.wangrl.workDemo;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println(ConstClass.HELLO);
 	}
 }
