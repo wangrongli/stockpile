9e6b5ac0328356fd0e706088d685df29be3c710e
wangrongli <wangrongli@daojia-inc.com>
2018-06-17 11:49:39 +0800

    test

---
+++ workDemo/src/main/java/com/wangrl/workDemo/ConstClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.wangrl.workDemo;
+
+  
+/**  
+ * @Description: TODO 
+ * @author wangrongli  
+ * @date 2018年6月17日  
+ */
+
+public class ConstClass {
+	static {
+		System.out.println("constClass init...");
+	}
+	public static final String HELLO = "hello";
+}
+
