c8548c768ba017336ba3fd72155307aea02f3d38
wangrongli <wangrongli@daojia-inc.com>
2019-01-29 00:29:40 +0800

    选择排序

---
+++ workDemo/src/main/java/com/wangrl/workDemo/algorithm/SortUtils.java
@@ -0,0 +1,26 @@
+package com.wangrl.workDemo.algorithm;
+
+public class SortUtils {
+    public static final int[] data = {6,10,113,22,9,24,11};
+    public static void selectSort(){
+        int length = data.length;
+        for(int i = 0;i<length;i++){
+            int min = data[i];
+            int minIndex = i;
+            for(int j=i+1;j<length;j++){
+                if (data[j] < min){
+                    min = data[j];
+                    minIndex = j;
+                }
+            }
+            data[minIndex] = data[i];
+            data[i] = min;
+        }
+    }
+    public static void main(String[] args){
+        selectSort();
+        for (int temp : data){
+            System.out.print(temp+" ");
+        }
+    }
+}
